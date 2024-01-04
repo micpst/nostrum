8c3ba4d509b6452bc695922f9c72b7da753a7b79
Michał Pstrąg <m.pstrag.kontakt@gmail.com>
2024-01-04 15:31:47 +0100

    add render replies

--- app/n/[note]/page.tsx
+++ app/n/[note]/page.tsx
@@ -1,16 +1,43 @@
 "use client";
 
 import Note from "@/app/components/note/note";
+import Error from "@/app/components/ui/error";
+import Loading from "@/app/components/ui/loading";
 import { useThread } from "@/app/lib/context/thread-provider";
+import { useEvents } from "@/app/lib/hooks/useEvents";
+import { useInfiniteScroll } from "@/app/lib/hooks/useInfiniteScroll";
+import { useNotesData } from "@/app/lib/hooks/useNotesData";
 
 function NotePage(): JSX.Element | undefined {
   const { root } = useThread();
+
+  const {
+    state: { events: notes, newEvents: newNotes, isLoading },
+    loadMoreRef,
+  } = useInfiniteScroll({
+    filter: { kinds: [1], "#e": [root?.id || ""] },
+    loadHook: useEvents,
+  });
+
+  useNotesData({ notes, newNotes });
 
   if (!root) return undefined;
 
   return (
     <>
       <Note event={root} />
+      {!isLoading && !notes.length ? (
+        <Error message="No replies" />
+      ) : (
+        notes.map((note, i) =>
+          i === notes.length - 5 ? (
+            <Note ref={loadMoreRef} key={note.id} event={note} />
+          ) : (
+            <Note key={note.id} event={note} />
+          )
+        )
+      )}
+      {isLoading ? <Loading className="my-5" /> : undefined}
     </>
   );
 }
