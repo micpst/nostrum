179776d897e5ca24f5b445f574af999e350f97fa
Michał Pstrąg <47692610+micpst@users.noreply.github.com>
2024-01-27 16:17:36 +0100

    feature: improve suggestions (#21)

--- app/lib/context/suggestion-provider.tsx
+++ app/lib/context/suggestion-provider.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-
 "use client";
 
 import { createContext, useContext, useEffect, useState } from "react";
 import { useDeepCompareEffect } from "react-use";
-import { DEFAULT_FOLLOWINGS } from "@/app/lib/constants";
+import { DEFAULT_SUGGESTIONS } from "@/app/lib/constants";
+import { useFollowing } from "@/app/lib/context/following-provider";
 import { useProfile } from "@/app/lib/context/profile-provider";
 import { useRelay } from "@/app/lib/context/relay-provider";
+import suggestionService from "@/app/lib/services/suggestionService";
 import type { ProviderProps } from "@/app/lib/context/providers";
 
 type SuggestionContext = {
@@ -17,20 +18,41 @@ type SuggestionContext = {
 export const SuggestionContext = createContext<SuggestionContext | null>(null);
 
 export default function SuggestionProvider({ children }: ProviderProps) {
+  const { following } = useFollowing();
+  const { add, remove } = useProfile();
   const { relays } = useRelay();
-  const { add, remove, reload } = useProfile();
 
-  const [suggestions, setSuggestions] = useState<string[]>(DEFAULT_FOLLOWINGS);
+  const [suggestions, setSuggestions] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useDeepCompareEffect(() => {
+    const fetchSuggestions = async (): Promise<void> => {
+      setIsLoading(true);
+
+      const suggestions = await suggestionService.listSuggestionsAsync({
+        relays: Array.from(relays.values()),
+        pubkeys: Array.from(following),
+      });
+
+      if (suggestions.length < 3) {
+        const defaultSuggestions = DEFAULT_SUGGESTIONS.filter(
+          (pubkey) => !following.has(pubkey)
+        );
+        suggestions.push(
+          ...defaultSuggestions.slice(0, 3 - suggestions.length)
+        );
+      }
+
+      setSuggestions(suggestions);
+      setIsLoading(false);
+    };
+    void fetchSuggestions();
+  }, [Array.from(following), Array.from(relays.keys())]);
 
   useEffect(() => {
     add(suggestions);
     return () => remove(suggestions);
-  }, []);
-
-  useDeepCompareEffect(() => {
-    void reload(suggestions);
-  }, [relays]);
+  }, [suggestions]);
 
   const value: SuggestionContext = {
     suggestions,
---
+++ app/lib/services/suggestionService.ts
@@ -0,0 +1,32 @@
+import followingService from "@/app/lib/services/followingService";
+import { findMostFrequent } from "@/app/lib/utils/common";
+import type { Relay } from "nostr-tools";
+
+export type ListSuggestionsRequest = {
+  relays: Relay[];
+  pubkeys: string[];
+};
+
+interface SuggestionService {
+  listSuggestionsAsync(request: ListSuggestionsRequest): Promise<string[]>;
+}
+
+async function listSuggestionsAsync({
+  relays,
+  pubkeys,
+}: ListSuggestionsRequest): Promise<string[]> {
+  const following = await Promise.all(
+    pubkeys.map((pubkey) =>
+      followingService.listFollowingAsync({ relays, pubkey })
+    )
+  );
+  return findMostFrequent(
+    following.flat().filter((pubkey) => !pubkeys.includes(pubkey))
+  );
+}
+
+const SuggestionService: SuggestionService = {
+  listSuggestionsAsync,
+};
+
+export default SuggestionService;
